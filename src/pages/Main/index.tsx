660f4a667a46348b9deb7f91cb3d8b1d81f30fc0
Debora Souza <debora.souzalima22@gmail.com>
2024-02-05 13:27:18 -0300

    Criação do componente Verticalbar, para o menu de Login e Cadastr-se. Por enquanto os links não enviam para nehuma página.

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from '../../components/Header'
+import { Verticalbar } from '../../components/Verticalbar'
 import { Container } from './styles'
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Verticalbar />
     </Container>
   )
 }
