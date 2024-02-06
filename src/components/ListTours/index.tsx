c4f7d23b22eaa06a3b1b4743169f0faa779bb75c
Debora Souza <debora.souzalima22@gmail.com>
2024-02-06 16:46:41 -0300

    Criação do componente List of Tours (Lista de Passeios).

---
+++ src/components/ListTours/index.tsx
@@ -0,0 +1,13 @@
+import {Container, Table} from './styles'
+
+
+export function ListTours () {
+  return <Container>
+    <>
+     <h1>Passeios Realizados</h1>
+      <Table>
+
+      </Table>
+    </>
+  </Container>
+}
---
+++ src/components/ListTours/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 80vh;
+  border: thin solid yellow;
+
+
+`
+
+export const Table = styled.table`
+
+`
